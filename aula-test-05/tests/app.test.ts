374005156d1c4dac97f63c62979962130328e258
monique <moniquejp16@gmail.com>
2023-09-27 03:04:44 -0300

    feat: aula de test 5, vou precisar da aula pra ver se ta certo

---
+++ aula-test-05/tests/app.test.ts
@@ -0,0 +1,20 @@
+import supertest from "supertest";
+
+import app from "./../src/app";
+
+const api = supertest(app);
+
+describe("API test", () => {
+  it("should return 200 when ask /health", async () => {
+    const { status, text } = await api.get("/health");
+    expect(status).toBe(200);
+    expect(text).toBe("OK!");
+  })
+
+  it("validar o array", async () => {
+    const result = await api.get("/fibonacci");
+    console.log(result)
+    expect(result.status).toBe(400);
+    expect(result.body).toStrictEqual({});
+  })
+})
